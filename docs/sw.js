394eff20a53bfa024e7f1a4eba655585778dab36
wlbkobashi-svg <wlb.kobashi@gmail.com>
2026-08-28 16:49:40 +0900

    Refresh cache after height field fix

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE='steel-floor-standalone-v2';
+const CACHE='steel-floor-standalone-v3';
 const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
